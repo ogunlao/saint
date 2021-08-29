292ccb32a9aba458318c1cda096ae909c6995dc8
ogunlao <ogunlao@yahoo.com>
2021-08-29 22:57:09 +0000

    Refactor util function for model loading

--- utils/utils.py
+++ utils/utils.py
@@ -13,32 +13,18 @@ def clones(module, N):
     "Produce N identical layers."
     return nn.ModuleList([copy.deepcopy(module) for _ in range(N)])
 
-def load_pretrained_transformer(transformer, path):
-    print('Loading pretrained transformer encoder...')
+def load_pretrained_model(model, path, model_name='transformer'):
+    print(f'Loading pretrained {model_name}...')
     state_dict = torch.load(path)['state_dict']
 
     pretrained_dict = {}
     for name in state_dict.keys():
-        if name.startswith('transformer'):
+        if name.startswith(model_name):
             new_name = '.'.join(name.split('.')[1:])
             pretrained_dict[new_name] = state_dict[name]
     
-    transformer.load_state_dict(pretrained_dict)
-    return transformer
-
-def load_pretrained_embedding(embedding, path):
-    state_dict = torch.load(path)['state_dict']
-    
-    print('Loading pretrained tranformer embedding...')
-
-    pretrained_dict = {}
-    for name in state_dict.keys():
-        if name.startswith('embedding'):
-            new_name = '.'.join(name.split('.')[1:])
-            pretrained_dict[new_name] = state_dict[name]
-            
-    embedding.load_state_dict(pretrained_dict)
-    return embedding
+    model.load_state_dict(pretrained_dict)
+    return model
 
 
 class dotdict(dict):
